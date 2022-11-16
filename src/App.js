284782a592f26dc3a0a6c28d4d1236c134598667
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2022-11-17 02:24:36 +0900

    React components and states

--- src/App.js
+++ src/App.js
@@ -6,24 +6,10 @@ import "./App.css"
 
 export default class App extends React.Component {
   render() {
-    var students = [
-      { id: 1, name: "Anvarbek", status: "Student" },
-      { id: 2, name: "Olimjon", status: "worker" },
-      { id: 3, name: "Sardor", status: "upemployed" },
-      { id: 4, name: "Akbar", status: "CTO" },
-      { id: 5, name: "John", status: "Manager" },
-      { id: 6, name: "Alex", status: "Student" }
-    ];
     return (
       <div>
-        {students.map((value) => {
-          return (
-            <StudentFunction
-              id={value.id}
-              name={value.name}
-              status={value.status} />)
-        })
-        }
+        <StudentClass name="Anavarbek" age="23" />
+        {/* <div>some text</div> */}
       </div>
     );
   }
--- src/StudentClass.js
+++ src/StudentClass.js
@@ -1,32 +1,12 @@
 import React from "react";
 import "./App"
-class StudentClass extends React.Component {
-    render() {
-        return (
-            <div>
-                {/* <table border="1">
-                    <tr>
-                        <th>ID: </th>
-                        <th>Name: </th>
-                        <th>Status: </th>
-                    </tr>
-                    {this.props.data.map((value, index) => {
-                        return (
-                            <tr>
-                                <td>{value.id}</td>
-                                <td>{value.name}</td>
-                                <td>{value.status}</td>
-                            </tr>
-                        )
-                    })}
-                </table> */}
-                <div className="container">
-                    <div>Id:  {this.props.id}</div>
-                    <div>Name: {this.props.name}</div>
-                    <div>status: {this.props.status}</div>
-                </div>
-            </div>
-        )
-    }
+function StudentClass(props) {
+    return (
+        <div>
+            <h1>Salom {props.name}</h1>
+            <p>Men {props.age}</p>
+            {props.children}
+        </div>
+    )
 }
 export default StudentClass;
